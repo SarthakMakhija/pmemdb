a7697c2fe9ced60e1a3d960de72ca76b2a69eeaa
sarthak.makhija <sarthak.makhija@gmail.com>
2022-01-05 11:41:20 +0530

    Sarthak | Adds a bootstrap test

---
+++ Bootstrap_test.go
@@ -0,0 +1,15 @@
+package pmemdb
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestBootstrap(t *testing.T) {
+	str, substr := "persistent memory", "persistent"
+	index := strings.Index(str, substr)
+
+	if index != 0 {
+		t.Fatalf("Expected %v at index 0 in %v but was %v", substr, str, index)
+	}
+}
