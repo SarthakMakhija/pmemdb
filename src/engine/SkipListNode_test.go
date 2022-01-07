ad671b8491e1d8b8fc2afe59e22142cbcfbc4b46
sarthak.makhija <sarthak.makhija@gmail.com>
2022-01-07 12:05:37 +0530

    Sarthak | Introduces "github.com/stretchr/testify/assert" in SkipListNode_test

--- src/engine/SkipListNode_test.go
+++ src/engine/SkipListNode_test.go
@@ -1,50 +1,43 @@
 package engine
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestShouldMatchKeyInSkipListNode(t *testing.T) {
 	node := newNode([]byte("HDD"), []byte("Hard disk drive"))
 	matches := node.matchesKey([]byte("HDD"))
 
-	if matches != true {
-		t.Fatalf("Expected key %v to match", "HDD")
-	}
+	assert.Truef(t, matches, "Expected key %v to match", "HDD")
 }
 
 func TestShouldNotMatchKeyInSkipListNode(t *testing.T) {
 	node := newNode([]byte("SDD"), []byte("Hard disk drive"))
 	matches := node.matchesKey([]byte("HDD"))
 
-	if matches != false {
-		t.Fatalf("Expected key %v to not match", "HDD")
-	}
+	assert.Falsef(t, matches, "Expected key %v to not match", "HDD")
 }
 
 func TestShouldReturnTrueGivenKeyIsEqualToNodesKey(t *testing.T) {
 	node := newNode([]byte("HDD"), []byte("Hard disk drive"))
 	isLessOrEqualTo := node.isKeyLessEqualTo([]byte("HDD"))
 
-	if isLessOrEqualTo != true {
-		t.Fatalf("Expected key %v to be equal to node's key", "HDD")
-	}
+	assert.Truef(t, isLessOrEqualTo, "Expected key %v to be equal to node's key", "HDD")
 }
 
 func TestShouldReturnTrueGivenNodeKeyIsLessThanGivenKey(t *testing.T) {
 	node := newNode([]byte("HDD"), []byte("Hard disk drive"))
 	isLessOrEqualTo := node.isKeyLessEqualTo([]byte("SDD"))
 
-	if isLessOrEqualTo != true {
-		t.Fatalf("Expected node's key to be less than given key %v", "HDD")
-	}
+	assert.Truef(t, isLessOrEqualTo, "Expected node's key to be less than given key %v", "HDD")
 }
 
 func TestShouldAddANewNodeToTheRight(t *testing.T) {
 	node := newNode([]byte("HDD"), []byte("Hard disk drive"))
 	newNode := node.addToRightWith([]byte("SDD"), []byte("Solid state drive"))
 
-	if node.right != newNode {
-		t.Fatalf("Expected node's right to be %v, received %v", newNode, node.right)
-	}
+	assert.Equalf(t, newNode, node.right, "Expected node's right to be %v, received %v", newNode, node.right)
 }
 
 func TestShouldUpdateDownPointerInTheNode(t *testing.T) {
@@ -52,9 +45,7 @@ func TestShouldUpdateDownPointerInTheNode(t *testing.T) {
 	down := newNode([]byte("SDD"), []byte("Solid state drive"))
 	node.updateDown(down)
 
-	if node.down != down {
-		t.Fatalf("Expected node's down to be %v, received %v", down, node.down)
-	}
+	assert.Equalf(t, down, node.down, "Expected node's down to be %v, received %v", down, node.down)
 }
 
 func TestShouldUpdateRightPointerInTheNode(t *testing.T) {
@@ -62,7 +53,5 @@ func TestShouldUpdateRightPointerInTheNode(t *testing.T) {
 	right := newNode([]byte("SDD"), []byte("Solid state drive"))
 	node.updateRight(right)
 
-	if node.right != right {
-		t.Fatalf("Expected node's right to be %v, received %v", right, node.right)
-	}
+	assert.Equalf(t, right, node.right, "Expected node's right to be %v, received %v", right, node.right)
 }
