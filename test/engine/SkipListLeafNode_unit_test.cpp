d2ca0995af9a179f32964233a2f310469709eabe
sarthak.makhija <sarthak.makhija@gmail.com>
2022-01-28 18:12:51 +0530

    Sarthak | Adds tests for iteration on leaf node

--- test/engine/SkipListLeafNode_unit_test.cpp
+++ test/engine/SkipListLeafNode_unit_test.cpp
@@ -35,3 +35,83 @@ TEST(SkipListLeafNode, RightKeyValuePairGivenRightPointerIsNull) {
   
   ASSERT_EQ(KeyValuePair("", ""), node -> rightKeyValuePair());
 }
+
+TEST(SkipListLeafNode, IterateAndReturnNodeWithMatchingKeyValue) {
+  SkipListLeafNode *node = new SkipListLeafNode("HDD", "Hard disk drive");
+  SkipListLeafNode *right = new SkipListLeafNode("Pmem", "Persistent Storage");
+  
+  node  -> updateRight(right);
+  right -> updateRight(new SkipListLeafNode("SDD", "Solid state drive"));
+  
+  string key = "SDD";
+  SkipListNode* targetNode = node -> iterate(key,
+                  [key] (SkipListNode* node) -> pair<SkipListNode*, bool> {
+                        if (node -> matchesKey(key)) {
+                          return make_pair(node, true);
+                        } else {
+                          return make_pair(nullptr, false);
+                        }  
+                    });
+  
+  ASSERT_EQ(KeyValuePair("SDD", "Solid state drive"), targetNode -> getKeyValuePair());
+}
+
+TEST(SkipListLeafNode, IterateAndReturnNodeWithoutMatchingKeyValue) {
+  SkipListLeafNode *node = new SkipListLeafNode("HDD", "Hard disk drive");
+  SkipListLeafNode *right = new SkipListLeafNode("Pmem", "Persistent Storage");
+  
+  node  -> updateRight(right);
+  right -> updateRight(new SkipListLeafNode("SDD", "Solid state drive"));
+  
+  string key = "Storage";
+  SkipListNode* targetNode = node -> iterate(key,
+                  [key] (SkipListNode* node) -> pair<SkipListNode*, bool> {
+                        if (node -> matchesKey(key)) {
+                          return make_pair(node, true);
+                        } else {
+                          return make_pair(nullptr, false);
+                        }  
+                    });
+  
+  ASSERT_EQ(nullptr, targetNode);
+}
+
+TEST(SkipListLeafNode, IterateAndReturnNodeAfterWhichKeyValueWouldBePut1) {
+  SkipListLeafNode *node = new SkipListLeafNode("HDD", "Hard disk drive");
+  SkipListLeafNode *right = new SkipListLeafNode("Pmem", "Persistent Storage");
+  
+  node  -> updateRight(right);
+  right -> updateRight(new SkipListLeafNode("SDD", "Solid state drive"));
+  
+  string key = "Rocks";
+  SkipListNode* targetNode = node -> iterate(key,
+                  [key] (SkipListNode* node) -> pair<SkipListNode*, bool> {
+                        if (node -> matchesKey(key)) {
+                          return make_pair(node, true);
+                        } else {
+                          return make_pair(nullptr, false);
+                        }  
+                    });
+  
+  ASSERT_EQ(KeyValuePair("Pmem", "Persistent Storage"), targetNode -> getKeyValuePair());
+}
+
+TEST(SkipListLeafNode, IterateAndReturnNodeAfterWhichKeyValueWouldBePut2) {
+  SkipListLeafNode *node = new SkipListLeafNode("HDD", "Hard disk drive");
+  SkipListLeafNode *right = new SkipListLeafNode("Pmem", "Persistent Storage");
+  
+  node  -> updateRight(right);
+  right -> updateRight(new SkipListLeafNode("SDD", "Solid state drive"));
+  
+  string key = "Tuff";
+  SkipListNode* targetNode = node -> iterate(key,
+                  [key] (SkipListNode* node) -> pair<SkipListNode*, bool> {
+                        if (node -> matchesKey(key)) {
+                          return make_pair(node, true);
+                        } else {
+                          return make_pair(nullptr, false);
+                        }  
+                    });
+  
+  ASSERT_EQ(KeyValuePair("SDD", "Solid state drive"), targetNode -> getKeyValuePair());
+}
