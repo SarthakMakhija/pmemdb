3999e84f72080350558e6af4db9ffd38d4abe0f5
sarthak.makhija <sarthak.makhija@gmail.com>
2022-01-29 00:00:57 +0530

    Sarthak | Adds an integration test with skiplist tower size as 1

--- test/engine/SkipList_integration_test.cpp
+++ test/engine/SkipList_integration_test.cpp
@@ -3,8 +3,26 @@
 #include "../../src/engine/SkipList.h"
 
 
-TEST(SkipListIntegration, CreateASkipListAndAdd1000KeyValuePairs) {
+TEST(SkipListIntegration, CreateASkipListAndAdd1000KeyValuePairsInSkipListWithTowerSize10) {
     SkipList* skipList = new SkipList(10);
+    for (int count = 1; count <= 1000; count++) {
+        string key   = "Key-"    + std::to_string(count);
+        string value = "Value-"  + std::to_string(count);
+
+        skipList -> put(key, value);
+    }
+    for (int count = 1; count <= 1000; count++) {
+        string key           = "Key-"   + std::to_string(count);
+        string expectedValue = "Value-" + std::to_string(count);
+        
+        pair<string, bool> existenceByValue = skipList -> get(key);
+        ASSERT_EQ(expectedValue, existenceByValue.first);
+        ASSERT_TRUE(existenceByValue.second);
+    }
+}
+
+TEST(SkipListIntegration, CreateASkipListAndAdd1000KeyValuePairsInSkipListWithTowerSize1) {
+    SkipList* skipList = new SkipList(1);
     for (int count = 1; count <= 1000; count++) {
         string key   = "Key-"    + std::to_string(count);
         string value = "Value-"  + std::to_string(count);
