a84c6e7db968e92cb1873e442f9fa4aa6702f9fe
sarthak.makhija <sarthak.makhija@gmail.com>
2022-01-30 12:10:14 +0530

    Sarthak | Adds an integration test for deletion

--- test/engine/SkipList_integration_test.cpp
+++ test/engine/SkipList_integration_test.cpp
@@ -86,3 +86,37 @@ TEST(SkipListIntegration, CreateASkipListAndUpdate1000KeyValuePairsUsingUpdateIn
         ASSERT_TRUE(existenceByValue.second);
     }
 }
+
+TEST(SkipListIntegration, CreateASkipListAndDeleteKeys) {
+    SkipList* skipList = new SkipList(10);
+    for (int count = 1; count <= 1000; count++) {
+        string key   = "Key-"    + std::to_string(count);
+        string value = "Value-"  + std::to_string(count);
+
+        skipList -> put(key, value);
+    }
+
+    skipList -> deleteBy("Key-"    + std::to_string(1));
+    for (int count = 900; count <= 1000; count++) {
+        string key   = "Key-"    + std::to_string(count);
+
+        skipList -> deleteBy(key);
+    }
+
+    for (int count = 2; count < 900; count++) {
+        string key           = "Key-"   + std::to_string(count);
+        string expectedValue = "Value-" + std::to_string(count);
+        
+        pair<string, bool> existenceByValue = skipList -> get(key);
+        ASSERT_EQ(expectedValue, existenceByValue.first);
+        ASSERT_TRUE(existenceByValue.second);
+    }
+
+    ASSERT_FALSE(skipList -> get("Key-"    + std::to_string(1)).second);
+    for (int count = 900; count <= 1000; count++) {
+        string key           = "Key-"   + std::to_string(count);
+        
+        pair<string, bool> existenceByValue = skipList -> get(key);
+        ASSERT_FALSE(existenceByValue.second);
+    }
+}
