d5add2edf0d4805668e9bd56544641d1ec9b7e39
sarthak <sarthak.makhija@gmail.com>
2022-02-08 18:53:44 +0530

    Sarthak | Renames test files

---
+++ test/engine/SkipList_functional_test.cpp
@@ -0,0 +1,142 @@
+#include <gtest/gtest.h>
+#include <string>
+#include "../../src/engine/SkipList.h"
+#include "./PersistentMemoryPoolFixture.h"
+
+TEST_F(PersistentMemoryPoolFixture, SkipListFunctional_CreateASkipListAndAdd500KeyValuePairsInSkipListWithTowerSize10) {
+    SkipList* skipList = new SkipList(10);
+    for (int count = 1; count <= 500; count++) {
+        std::string key   = "Key-"    + std::to_string(count);
+        std::string value = "Value-"  + std::to_string(count);
+
+        skipList -> put(key, value);
+    }
+    for (int count = 1; count <= 500; count++) {
+        std::string key           = "Key-"   + std::to_string(count);
+        std::string expectedValue = "Value-" + std::to_string(count);
+        
+        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
+        ASSERT_EQ(expectedValue, existenceByValue.first);
+        ASSERT_TRUE(existenceByValue.second);
+    }
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListFunctional_CreateASkipListAndAdd500KeyValuePairsInSkipListWithTowerSize1) {
+    SkipList* skipList = new SkipList(1);
+    for (int count = 1; count <= 500; count++) {
+        std::string key   = "Key-"    + std::to_string(count);
+        std::string value = "Value-"  + std::to_string(count);
+
+        skipList -> put(key, value);
+    }
+    for (int count = 1; count <= 500; count++) {
+        std::string key           = "Key-"   + std::to_string(count);
+        std::string expectedValue = "Value-" + std::to_string(count);
+        
+        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
+        ASSERT_EQ(expectedValue, existenceByValue.first);
+        ASSERT_TRUE(existenceByValue.second);
+    }
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListFunctional_CreateASkipListAndDoesAScan) {
+    SkipList* skipList = new SkipList(10);
+    for (int count = 1; count <= 500; count+=2) {
+        std::string key   =  std::to_string(count);
+        std::string value =  std::to_string(count);
+
+        skipList -> put(key, value);
+    }    
+    std::string beginKey = "50";
+    std::string endKey   = "70";
+
+    std::vector<KeyValuePair> expected;
+    for (int count = 51; count < 70; count+=2) {
+        expected.push_back(KeyValuePair(std::to_string(count), std::to_string(count)));
+    }
+    expected.push_back(KeyValuePair("7", "7"));
+
+    std::vector<KeyValuePair> pairs = skipList -> scan(beginKey, endKey, 50);
+    ASSERT_EQ(expected, pairs);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListFunctional_CreateASkipListAndDoesAScanWithMaxPairsAs5) {
+    SkipList* skipList = new SkipList(10);
+    for (int count = 1; count <= 500; count+=2) {
+        std::string key   =  std::to_string(count);
+        std::string value =  std::to_string(count);
+
+        skipList -> put(key, value);
+    }    
+    std::string beginKey = "50";
+    std::string endKey   = "70";
+
+    std::vector<KeyValuePair> expected;
+    for (int count = 51; count <= 59; count+=2) {
+        expected.push_back(KeyValuePair(std::to_string(count), std::to_string(count)));
+    }
+
+    std::vector<KeyValuePair> pairs = skipList -> scan(beginKey, endKey, 5);
+    ASSERT_EQ(expected, pairs);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListFunctional_CreateASkipListAndUpdate500KeyValuePairsUsingUpdateInSkipListWithTowerSize10) {
+    SkipList* skipList = new SkipList(10);
+    for (int count = 1; count <= 500; count++) {
+        std::string key   = "Key-"    + std::to_string(count);
+        std::string value = "Value-"  + std::to_string(count);
+
+        skipList -> put(key, value);
+    }
+    for (int count = 1; count <= 500; count++) {
+        std::string key   = "Key-"    + std::to_string(count);
+        std::string value = "Value-"  + std::to_string(count*2);
+
+        skipList -> update(key, value);
+    }
+    for (int count = 1; count <= 500; count++) {
+        std::string key           = "Key-"   + std::to_string(count);
+        std::string expectedValue = "Value-" + std::to_string(count*2);
+        
+        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
+        ASSERT_EQ(expectedValue, existenceByValue.first);
+        ASSERT_TRUE(existenceByValue.second);
+    }
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListFunctional_CreateASkipListAndDeleteKeys) {
+    SkipList* skipList = new SkipList(10);
+    for (int count = 1; count <= 500; count++) {
+        std::string key   = "Key-"    + std::to_string(count);
+        std::string value = "Value-"  + std::to_string(count);
+
+        skipList -> put(key, value);
+    }
+
+    skipList -> deleteBy("Key-"    + std::to_string(1));
+    for (int count = 500; count <= 400; count++) {
+        std::string key   = "Key-"    + std::to_string(count);
+
+        skipList -> deleteBy(key);
+    }
+
+    for (int count = 2; count < 400; count++) {
+        std::string key           = "Key-"   + std::to_string(count);
+        std::string expectedValue = "Value-" + std::to_string(count);
+        
+        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
+
+        ASSERT_EQ(expectedValue, existenceByValue.first);
+        ASSERT_TRUE(existenceByValue.second);
+    }
+
+    ASSERT_FALSE(skipList -> get("Key-"    + std::to_string(1)).second);
+    for (int count = 500; count <= 400; count++) {
+        std::string key           = "Key-"   + std::to_string(count);
+        
+        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
+
+        ASSERT_EQ("", existenceByValue.first);
+        ASSERT_FALSE(existenceByValue.second);
+    }
+}
--- test/engine/SkipList_integration_test.cpp
+++ test/engine/SkipList_integration_test.cpp
@@ -3,140 +3,182 @@
 #include "../../src/engine/SkipList.h"
 #include "./PersistentMemoryPoolFixture.h"
 
-TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndAdd500KeyValuePairsInSkipListWithTowerSize10) {
-    SkipList* skipList = new SkipList(10);
-    for (int count = 1; count <= 500; count++) {
-        std::string key   = "Key-"    + std::to_string(count);
-        std::string value = "Value-"  + std::to_string(count);
 
-        skipList -> put(key, value);
-    }
-    for (int count = 1; count <= 500; count++) {
-        std::string key           = "Key-"   + std::to_string(count);
-        std::string expectedValue = "Value-" + std::to_string(count);
-        
-        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
-        ASSERT_EQ(expectedValue, existenceByValue.first);
-        ASSERT_TRUE(existenceByValue.second);
-    }
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndGetAValueByKey) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+
+    std::pair<std::string, bool> existenceByValue = skipList -> get("HDD");
+    ASSERT_EQ("Hard disk drive", existenceByValue.first);
 }
 
-TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndAdd500KeyValuePairsInSkipListWithTowerSize1) {
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndGetExistenceOfKey) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+
+    std::pair<std::string, bool> existenceByValue = skipList -> get("HDD");
+    ASSERT_TRUE(existenceByValue.second);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndGetAValueByNonExistentKey) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+
+    std::pair<std::string, bool> existenceByValue = skipList -> get("SDD");
+    
+    ASSERT_EQ("", existenceByValue.first);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndGetTheExistenceOfNonExistentKey) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+
+    std::pair<std::string, bool> existenceByValue = skipList -> get("SDD");
+    
+    ASSERT_FALSE(existenceByValue.second);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDoesMultiGet) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+    skipList -> put("SDD", "Solid state drive");
+
+    std::vector<std::string> keys = {"HDD", "SDD", "Pmem", "DoesNotExist"};
+    std::vector<std::pair<std::string, bool>> result = skipList -> multiGet(keys);
+    std::vector<std::pair<std::string, bool>> expected = {
+                            std::make_pair("Hard disk drive", true), 
+                            std::make_pair("Solid state drive", true),
+                            std::make_pair("Persistent Memory", true),
+                            std::make_pair("", false)
+    };
+    
+    ASSERT_EQ(expected, result);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_ScanWithBeginKeyPresent) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+    skipList -> put("SDD", "Solid state drive");
+    skipList -> put("RAM", "Random access memory");
+
+    std::string beginKey = "Pmem";
+    std::string endKey = "SDD";
+
+    std::vector<KeyValuePair> pairs = skipList -> scan(beginKey, endKey, 10);
+    std::vector<KeyValuePair> expected = {KeyValuePair("Pmem", "Persistent Memory"), KeyValuePair("RAM", "Random access memory")};
+    
+    ASSERT_EQ(expected, pairs);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_ScanWithBeginKeyNotPresent) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("SDD", "Solid state drive");
+    skipList -> put("RAM", "Random access memory");
+
+    std::string beginKey = "Pmem";
+    std::string endKey = "SDD";
+
+    std::vector<KeyValuePair> pairs = skipList -> scan(beginKey, endKey, 10);
+    std::vector<KeyValuePair> expected = {KeyValuePair("RAM", "Random access memory")};
+    
+  ASSERT_EQ(expected, pairs);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndUpdateAValue) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+
+    skipList -> update("HDD", "Hard disk");
+    std::pair<std::string, bool> existenceByValue = skipList -> get("HDD");
+    ASSERT_EQ("Hard disk", existenceByValue.first);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndUpdateAValueForNonExistingKey) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+
+    skipList -> update("SDD", "Solid state drive");
+    std::pair<std::string, bool> existenceByValue = skipList -> get("SDD");
+    ASSERT_EQ("", existenceByValue.first);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDeleteByAKeyInTheBeginning) {
     SkipList* skipList = new SkipList(1);
-    for (int count = 1; count <= 500; count++) {
-        std::string key   = "Key-"    + std::to_string(count);
-        std::string value = "Value-"  + std::to_string(count);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
 
-        skipList -> put(key, value);
+    skipList -> deleteBy("HDD");
+    std::pair<std::string, bool> existenceByValue = skipList -> get("HDD");
+    ASSERT_EQ("", existenceByValue.first);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDeleteByAKeyInBetween) {
+    SkipList* skipList = new SkipList(1);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+    skipList -> put("SDD", "Solid state drive");
+
+    skipList -> deleteBy("Pmem");
+    std::pair<std::string, bool> existenceByValue = skipList -> get("Pmem");
+    ASSERT_EQ("", existenceByValue.first);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDeleteByAKeyInTheEnd) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+
+    skipList -> deleteBy("Pmem");
+    std::pair<std::string, bool> existenceByValue = skipList -> get("Pmem");
+    ASSERT_EQ("", existenceByValue.first);
+}
+
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDeleteRangeWithBeginKeyPresent) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("A", "A");
+    skipList -> put("B", "B");
+    skipList -> put("C", "C");
+    skipList -> put("D", "D");
+    skipList -> put("E", "E");
+
+    skipList -> deleteRange("B", "D");
+    
+    std::vector<std::string> missingKeys = {"B", "C"};
+    for (auto missingKey: missingKeys) {
+        ASSERT_EQ("",  skipList -> get(missingKey).first);
     }
-    for (int count = 1; count <= 500; count++) {
-        std::string key           = "Key-"   + std::to_string(count);
-        std::string expectedValue = "Value-" + std::to_string(count);
-        
-        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
-        ASSERT_EQ(expectedValue, existenceByValue.first);
-        ASSERT_TRUE(existenceByValue.second);
+
+    std::vector<std::string> presentKeys = {"A", "D", "E"};
+    for (auto presentKey: presentKeys) {
+        ASSERT_EQ(presentKey, skipList -> get(presentKey).first);
     }
 }
 
-TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDoesAScan) {
-    SkipList* skipList = new SkipList(10);
-    for (int count = 1; count <= 500; count+=2) {
-        std::string key   =  std::to_string(count);
-        std::string value =  std::to_string(count);
+TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDeleteRangeWithBeginKeyNotPresent) {
+    SkipList* skipList = new SkipList(5);
+    skipList -> put("A", "A");
+    skipList -> put("B", "B");
+    skipList -> put("D", "D");
+    skipList -> put("E", "E");
 
-        skipList -> put(key, value);
-    }    
-    std::string beginKey = "50";
-    std::string endKey   = "70";
-
-    std::vector<KeyValuePair> expected;
-    for (int count = 51; count < 70; count+=2) {
-        expected.push_back(KeyValuePair(std::to_string(count), std::to_string(count)));
-    }
-    expected.push_back(KeyValuePair("7", "7"));
-
-    std::vector<KeyValuePair> pairs = skipList -> scan(beginKey, endKey, 50);
-    ASSERT_EQ(expected, pairs);
-}
-
-TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDoesAScanWithMaxPairsAs5) {
-    SkipList* skipList = new SkipList(10);
-    for (int count = 1; count <= 500; count+=2) {
-        std::string key   =  std::to_string(count);
-        std::string value =  std::to_string(count);
-
-        skipList -> put(key, value);
-    }    
-    std::string beginKey = "50";
-    std::string endKey   = "70";
-
-    std::vector<KeyValuePair> expected;
-    for (int count = 51; count <= 59; count+=2) {
-        expected.push_back(KeyValuePair(std::to_string(count), std::to_string(count)));
+    skipList -> deleteRange("C", "E");
+    
+    std::vector<std::string> missingKeys = {"D"};
+    for (auto missingKey: missingKeys) {
+        ASSERT_EQ("",  skipList -> get(missingKey).first);
     }
 
-    std::vector<KeyValuePair> pairs = skipList -> scan(beginKey, endKey, 5);
-    ASSERT_EQ(expected, pairs);
-}
-
-TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndUpdate500KeyValuePairsUsingUpdateInSkipListWithTowerSize10) {
-    SkipList* skipList = new SkipList(10);
-    for (int count = 1; count <= 500; count++) {
-        std::string key   = "Key-"    + std::to_string(count);
-        std::string value = "Value-"  + std::to_string(count);
-
-        skipList -> put(key, value);
-    }
-    for (int count = 1; count <= 500; count++) {
-        std::string key   = "Key-"    + std::to_string(count);
-        std::string value = "Value-"  + std::to_string(count*2);
-
-        skipList -> update(key, value);
-    }
-    for (int count = 1; count <= 500; count++) {
-        std::string key           = "Key-"   + std::to_string(count);
-        std::string expectedValue = "Value-" + std::to_string(count*2);
-        
-        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
-        ASSERT_EQ(expectedValue, existenceByValue.first);
-        ASSERT_TRUE(existenceByValue.second);
-    }
-}
-
-TEST_F(PersistentMemoryPoolFixture, SkipListIntegration_CreateASkipListAndDeleteKeys) {
-    SkipList* skipList = new SkipList(10);
-    for (int count = 1; count <= 500; count++) {
-        std::string key   = "Key-"    + std::to_string(count);
-        std::string value = "Value-"  + std::to_string(count);
-
-        skipList -> put(key, value);
-    }
-
-    skipList -> deleteBy("Key-"    + std::to_string(1));
-    for (int count = 500; count <= 400; count++) {
-        std::string key   = "Key-"    + std::to_string(count);
-
-        skipList -> deleteBy(key);
-    }
-
-    for (int count = 2; count < 400; count++) {
-        std::string key           = "Key-"   + std::to_string(count);
-        std::string expectedValue = "Value-" + std::to_string(count);
-        
-        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
-
-        ASSERT_EQ(expectedValue, existenceByValue.first);
-        ASSERT_TRUE(existenceByValue.second);
-    }
-
-    ASSERT_FALSE(skipList -> get("Key-"    + std::to_string(1)).second);
-    for (int count = 500; count <= 400; count++) {
-        std::string key           = "Key-"   + std::to_string(count);
-        
-        std::pair<std::string, bool> existenceByValue = skipList -> get(key);
-
-        ASSERT_EQ("", existenceByValue.first);
-        ASSERT_FALSE(existenceByValue.second);
+    std::vector<std::string> presentKeys = {"A", "B", "E"};
+    for (auto presentKey: presentKeys) {
+        ASSERT_EQ(presentKey, skipList -> get(presentKey).first);
     }
 }
