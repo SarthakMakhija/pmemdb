71541c5373dd4386532f5af2ca9913308e883a8a
sarthak <sarthak.makhija@gmail.com>
2022-03-01 20:44:37 +0530

    Sarthak | Adds SkipList_concurrent_integration_test.cpp

---
+++ test/engine/SkipList_concurrent_integration_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <thread>
+#include "../../src/engine/SkipList.h"
+#include "./PersistentMemoryPoolFixture.h"
+
+
+TEST_F(PersistentMemoryPoolFixture, SkipListConcurrentIntegration_TwoThreadsReadingTheSameKey) {
+    SkipList* skipList = new SkipList(8, 0.5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("Pmem", "Persistent Memory");
+
+    std::thread reader1([&]() {
+        ASSERT_EQ("Hard disk drive", skipList -> get("HDD").first);
+    });
+
+    std::thread reader2([&]() {
+        ASSERT_EQ("Hard disk drive", skipList -> get("HDD").first);
+    });
+
+    reader1.join();
+    reader2.join();
+}
