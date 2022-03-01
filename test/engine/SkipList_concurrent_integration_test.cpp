315356fbb0c6104e0c16e50f88ef4be221fec874
sarthak <sarthak.makhija@gmail.com>
2022-03-01 22:12:36 +0530

    Sarthak | Adds SkipListConcurrentIntegration test for multiGet

--- test/engine/SkipList_concurrent_integration_test.cpp
+++ test/engine/SkipList_concurrent_integration_test.cpp
@@ -39,3 +39,32 @@ TEST_F(PersistentMemoryPoolFixture, SkipListConcurrentIntegration_TwoThreadsRead
     reader1.join();
     reader2.join();
 }
+
+TEST_F(PersistentMemoryPoolFixture, SkipListConcurrentIntegration_TwoThreadsPerformingMultiGet) {
+    SkipList* skipList = new SkipList(8, 0.5);
+    skipList -> put("HDD", "Hard disk drive");
+    skipList -> put("SDD", "Solid state drive");
+    skipList -> put("Pmem", "Persistent Memory");
+    skipList -> put("Nvm", "Non volatile memory");
+
+    std::thread reader1([&]() {
+        std::vector<std::pair<std::string, bool>> expected = {
+                        std::make_pair("Hard disk drive", true), 
+                        std::make_pair("Solid state drive", true)
+        };
+        std::vector<std::string> keys = {"HDD", "SDD"};
+        ASSERT_EQ(expected, skipList -> multiGet(keys));
+    });
+
+    std::thread reader2([&]() {
+        std::vector<std::pair<std::string, bool>> expected = {
+                        std::make_pair("Non volatile memory", true), 
+                        std::make_pair("Persistent Memory", true)
+        };
+        std::vector<std::string> keys = {"Pmem", "Nvm"};
+        ASSERT_EQ(expected, skipList -> multiGet(keys));
+    });
+
+    reader1.join();
+    reader2.join();
+}
