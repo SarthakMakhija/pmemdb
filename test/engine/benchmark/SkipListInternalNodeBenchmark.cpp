00747ef0017dbc80b9c704642ced5df92594d3e4
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-04-04 18:00:45 +0530

    Sarthak | Adds benchmark for scan at the level of SkipListInternalNode

--- test/engine/benchmark/SkipListInternalNodeBenchmark.cpp
+++ test/engine/benchmark/SkipListInternalNodeBenchmark.cpp
@@ -78,6 +78,50 @@ static void SkipListInternalNodePutPositionArguments(benchmark::internal::Benchm
     b->ArgNames({"maxKey", "perKeySize"});
 }
 
+static void SkipListInternalNodePut(benchmark::State &state) {
+    uint64_t maxKey = state.range(0);
+    uint64_t perKeySize = state.range(1);
+    KeyComparator *keyComparator = new UInt32KeyComparator();
+
+    auto rnd = Random(301 + state.thread_index());
+    KeyGenerator kg(&rnd);
+
+    static SkipListInternalNode *skipListInternalNode = nullptr;
+    static PersistentMemoryPool *pool = nullptr;
+    if (state.thread_index() == 0) {
+        auto nodePoolPair = SetUpSkipListInternalNode();
+        skipListInternalNode = nodePoolPair.first;
+        pool = nodePoolPair.second;
+    }
+
+    for (auto _: state) {
+        state.PauseTiming();
+        Slice slice = kg.Next();
+        char *key = slice.buff;
+        state.ResumeTiming();
+
+        PutPosition putPosition = skipListInternalNode->putPositionOf(key, keyComparator);
+        if (putPosition.newLevel != -1) {
+            SkipListNode *newInternal = skipListInternalNode->put(key, putPosition.positions, putPosition.newLevel);
+            if (newInternal == nullptr) {
+                state.SkipWithError("Returned nullptr as the new node");
+            }
+        }
+    }
+
+    if (state.thread_index() == 0) {
+        TearDownSkipListInternalNode(pool);
+    }
+}
+
+static void SkipListInternalNodePutArguments(benchmark::internal::Benchmark *b) {
+    for (int64_t maxKey: {100l << 30}) {
+        for (int64_t perKeySize: {256, 1024}) {
+            b->Args({maxKey, perKeySize});
+        }
+    }
+    b->ArgNames({"maxKey", "perKeySize"});
+}
 
 static void SkipListInternalNodeGet(benchmark::State &state) {
     uint64_t maxKey = state.range(0);
@@ -138,52 +182,68 @@ static void SkipListInternalNodeGetArguments(benchmark::internal::Benchmark *b) 
     b->ArgNames({"maxKey", "perKeySize", "negativeQuery"});
 }
 
-static void SkipListInternalNodePut(benchmark::State &state) {
+
+static void SkipListInternalNodeScan(benchmark::State &state) {
     uint64_t maxKey = state.range(0);
     uint64_t perKeySize = state.range(1);
-    KeyComparator *keyComparator = new UInt32KeyComparator();
+    bool negativeQuery = state.range(2);
+
+    uint64_t numberOfKeys = maxKey / perKeySize;
 
     auto rnd = Random(301 + state.thread_index());
-    KeyGenerator kg(&rnd);
+    KeyGenerator kg(&rnd, numberOfKeys);
+    KeyComparator *keyComparator = new UInt32KeyComparator();
 
     static SkipListInternalNode *skipListInternalNode = nullptr;
     static PersistentMemoryPool *pool = nullptr;
+
     if (state.thread_index() == 0) {
         auto nodePoolPair = SetUpSkipListInternalNode();
         skipListInternalNode = nodePoolPair.first;
         pool = nodePoolPair.second;
-    }
 
-    for (auto _: state) {
-        state.PauseTiming();
-        Slice slice = kg.Next();
-        char *key = slice.buff;
-        state.ResumeTiming();
+        for (uint64_t count = 0; count < numberOfKeys; count++) {
+            Slice slice = kg.Next();
+            char *key = slice.buff;
 
-        PutPosition putPosition = skipListInternalNode->putPositionOf(key, keyComparator);
-        if (putPosition.newLevel != -1) {
-            SkipListNode *newInternal = skipListInternalNode->put(key, putPosition.positions, putPosition.newLevel);
-            if (newInternal == nullptr) {
-                state.SkipWithError("Returned nullptr as the new node");
+            PutPosition putPosition = skipListInternalNode->putPositionOf(key, keyComparator);
+            if (putPosition.newLevel != -1) {
+                SkipListNode *newInternal = skipListInternalNode->put(key, putPosition.positions, putPosition.newLevel);
+                if (newInternal == nullptr) {
+                    state.SkipWithError("Returned nullptr as the new node");
+                }
             }
         }
     }
 
+    size_t notFound = 0;
+    for (auto _: state) {
+        state.PauseTiming();
+        char *beginKey = kg.Next().buff;
+        state.ResumeTiming();
+        std::pair<SkipListNode*, bool> pair = skipListInternalNode->scan(beginKey, keyComparator);
+        if (!pair.second) {
+            notFound++;
+        }
+    }
     if (state.thread_index() == 0) {
         TearDownSkipListInternalNode(pool);
     }
 }
 
-static void SkipListInternalNodePutArguments(benchmark::internal::Benchmark *b) {
-    for (int64_t maxKey: {100l << 30}) {
-        for (int64_t perKeySize: {256, 1024}) {
-            b->Args({maxKey, perKeySize});
+static void SkipListInternalNodeScanArguments(benchmark::internal::Benchmark *b) {
+    for (int64_t maxKey: {512l << 20}) {
+        for (int64_t perKeySize: {1024}) {
+            for (bool negativeQuery: {false}) {
+                b->Args({maxKey, perKeySize, negativeQuery});
+            }
         }
     }
-    b->ArgNames({"maxKey", "perKeySize"});
+    b->ArgNames({"maxKey", "perKeySize", "negativeQuery"});
 }
 
 
 BENCHMARK(SkipListInternalNodePutPosition)->Apply(SkipListInternalNodePutPositionArguments);
 BENCHMARK(SkipListInternalNodePut)->Apply(SkipListInternalNodePutArguments);
 BENCHMARK(SkipListInternalNodeGet)->Apply(SkipListInternalNodeGetArguments);
+BENCHMARK(SkipListInternalNodeScan)->Apply(SkipListInternalNodeScanArguments);
