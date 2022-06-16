0d34845b8d7e1d38f0eccd24caa5aa39c1b6a939
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-06-16 12:44:03 +0530

    Sarthak | Adds functions.cmake

---
+++ cmake_modules/functions.cmake
@@ -0,0 +1,6 @@
+function(ADD_CXX_FLAGS FLAGS)
+  if ($ENV{YB_VERBOSE})
+    message("Adding C++ flags: ${FLAGS}")
+  endif()
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FLAGS}" PARENT_SCOPE)
+endfunction()
